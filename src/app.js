58b1be4a1a5976ec9507707f798456e0c64478b9
Sarah Gibbens <sarahekg@gmail.com>
2022-12-17 18:24:44 +0000

    fixes after watching solution

--- src/app.js
+++ src/app.js
@@ -6,13 +6,13 @@ const express = require('express');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('/', function (req, res) {
-    res.render('views', {title: "Index"});
+    return res.render('index', {title: "Index"});
 });
 
 app.listen(3000, function(err) {
